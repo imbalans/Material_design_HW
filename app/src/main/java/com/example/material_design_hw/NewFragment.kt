4d63357e5d466080a401d0bd554beb7ef17a11b7
Alexander Andreev <imba.yeah@yandex.ru>
2020-11-19 01:27:35 +0300

    hw_4

--- app/src/main/java/com/example/material_design_hw/NewFragment.kt
+++ app/src/main/java/com/example/material_design_hw/NewFragment.kt
@@ -1,19 +1,18 @@
-package com.example.material_design_hw.ui.gallery
+package com.example.material_design_hw
 
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.material_design_hw.R
 
-class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+class NewFragment : Fragment(R.layout.new_fragment){
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_home).setOnClickListener{
-            findNavController().navigate(R.id.action_nav_gallery_to_nav_home)
+        view.findViewById<Button>(R.id.button_gallery).setOnClickListener {
+            findNavController().navigate(R.id.action_newFragment_to_nav_gallery)
         }
     }
 }
--- app/src/main/java/com/example/material_design_hw/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/material_design_hw/ui/home/HomeFragment.kt
@@ -1,31 +1,20 @@
 package com.example.material_design_hw.ui.home
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.material_design_hw.R
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    private lateinit var homeViewModel: HomeViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        view.findViewById<Button>(R.id.button_slideshow).setOnClickListener {
+            findNavController().navigate(R.id.action_nav_home_to_nav_slideshow)
+
+        }
     }
 }
